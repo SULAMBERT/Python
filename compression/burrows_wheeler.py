cfff6d54c6504c76a98f60d5538336aa4f31839e
subhendu_ <subhendu.rana@round.glass>
2021-11-18 23:02:38 +0530

    My feature is ready

--- compression/burrows_wheeler.py
+++ compression/burrows_wheeler.py
@@ -46,7 +46,7 @@ def all_rotations(s: str) -> list[str]:
     """
     if not isinstance(s, str):
         raise TypeError("The parameter s type must be str.")
-
+    print("hellow")
     return [s[i:] + s[:i] for i in range(len(s))]
 
 
